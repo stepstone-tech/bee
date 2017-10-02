25fef17888ddc4a35eb66f08409beadd51fcef0e
Piotr Bandurski <bandup01@stepstone.com>
2017-10-02 09:18:30 +0200

    calling listener even the same value selected

---
+++ bee/src/main/java/com/orhanobut/bee/NDSpinner.java
@@ -0,0 +1,46 @@
+package com.orhanobut.bee;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Spinner;
+
+/**
+ * Spinner extension that calls onItemSelected even when the selection is the same as its previous value
+ * https://stackoverflow.com/questions/8937150/android-spinner-fire-event-when-same-item-selection-is-made
+ */
+public class NDSpinner extends Spinner {
+
+    public NDSpinner(Context context) {
+        super(context);
+    }
+
+    public NDSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NDSpinner(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void
+    setSelection(int position, boolean animate) {
+        boolean sameSelected = position == getSelectedItemPosition();
+        super.setSelection(position, animate);
+        if (sameSelected && getOnItemSelectedListener() != null) {
+            // Spinner does not call the OnItemSelectedListener if the same item is selected, so do it manually now
+            getOnItemSelectedListener().onItemSelected(this, getSelectedView(), position, getSelectedItemId());
+        }
+    }
+
+    @Override
+    public void
+    setSelection(int position) {
+        boolean sameSelected = position == getSelectedItemPosition();
+        super.setSelection(position);
+        if (sameSelected && getOnItemSelectedListener() != null) {
+            // Spinner does not call the OnItemSelectedListener if the same item is selected, so do it manually now
+            getOnItemSelectedListener().onItemSelected(this, getSelectedView(), position, getSelectedItemId());
+        }
+    }
+}
